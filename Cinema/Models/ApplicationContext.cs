50e5b18046a9efafd06a41b5cd426c7843cb5d37
Kamil Sabirov <ksabirov448@gmail.com>
2024-03-29 15:19:48 +0300

    Добавил класс Film, FilmRating (вспомогательный для Film). Добавил контекст базы данных

---
+++ Cinema/Models/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Models
+{
+    public class ApplicationContext
+    {
+        public DbSet<Film> Films { get; set; }
+        public DbSet<FilmRating> FilmRatings { get; set; }
+
+    }
+}
