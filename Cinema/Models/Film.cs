b52587646de1aac9ff79473732df0d5796a770a9
Kamil Sabirov <ksabirov448@gmail.com>
2024-03-29 15:19:48 +0300

    Добавил класс Film для описания одного фильма. Также добавил вспомогательный к нему FilmRating. Добавил контекст базы данных

---
+++ Cinema/Models/Film.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Models
+{
+    public class Film
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+        public string? Description { get; set; }
+        public List<FilmRating> Ratings { get; set; } = new();
+    }
+}
