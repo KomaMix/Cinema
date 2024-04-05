4aa1d7c1e78471d1b84488372ba35e4aded495e6
Kamil Sabirov <ksabirov448@gmail.com>
2024-04-05 18:44:30 +0300

    Внес небольшие изменения

--- Cinema/Models/SeedData.cs
+++ Cinema/Models/SeedData.cs
@@ -8,31 +8,62 @@
             {
                 var context = serviceScope.ServiceProvider.GetService<ApplicationContext>();
 
-                if (context != null && !context.Films.Any())
+                if (context == null || (context.FilmRatings.Any() && context.Films.Any()))
                 {
-                    context.Films.AddRange(
-                    new Film
+                    Film film1 = new Film
                     {
                         Name = "Аватар: Путь воды",
                         Director = "Джеймс Кэмерон",
                         Description = "После принятия образа аватара солдат Джейк Салли становится предводителем народа на'ви и берет на себя миссию по защите новых друзей от корыстных бизнесменов с Земли. Теперь ему есть за кого бороться — с Джейком его прекрасная возлюбленная Нейтири. Когда на Пандору возвращаются до зубов вооруженные земляне, Джейк готов дать им отпор."
-                    },
-                    new Film
+                    };
+                    Film film2 = new Film
                     {
                         Name = "Интерстеллар",
                         Director = "Кристофер Нолан",
                         Description = "Когда засуха, пыльные бури и вымирание растений приводят человечество к продовольственному кризису, коллектив исследователей и учёных отправляется сквозь червоточину (которая предположительно соединяет области пространства-времени через большое расстояние) в путешествие, чтобы превзойти прежние ограничения для космических путешествий человека и найти планету с подходящими для человечества условиями."
-                    },
-                    new Film
+                    };
+                    Film film3 = new Film
                     {
                         Name = "Твоё имя",
                         Director = "Макото Синкай",
                         Description = "Токийский парень Таки и провинциальная девушка Мицуха обнаруживают, что между ними существует странная связь. Во сне они меняются телами и проживают жизни друг друга. Но однажды эта способность исчезает так же внезапно, как появилась. Таки решает во что бы то ни стало отыскать Мицуху."
+                    };
+
+                    FilmRating rating1 = new FilmRating
+                    {
+                        Rating = 8,
+                        Film = film1
+                    };
+
+                    FilmRating rating2 = new FilmRating
+                    {
+                        Rating = 9,
+                        Film = film1
+                    };
+
+                    FilmRating rating3 = new FilmRating
+                    {
+                        Rating = 3,
+                        Film = film2
+                    };
+
+                    FilmRating rating4 = new FilmRating
+                    {
+                        Rating = 3,
+                        Film = film3
+                    };
+
+                    if (context != null)
+                    {
+                        context.Films.AddRange(film1, film2, film3);
+                        context.FilmRatings.AddRange(rating1, rating2, rating3, rating4);
+                        context.SaveChanges();
+                    } else
+                    {
+                        throw new Exception("База данных пуста");
                     }
-                    );
-                    context.SaveChanges();
+                    
                 }
-                
             }
         }
     }
