01926cbf2e9c56f6457580ca4c402bf0864cf83c
KomaMix <ksabirov448@gmail.com>
2024-04-09 19:40:02 +0300

    Добавил контроллер регистрации, а также простые view к нему

---
+++ Cinema/Controllers/AdminController.cs
@@ -0,0 +1,51 @@
+﻿using Cinema.Models;
+using Cinema.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cinema.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.Name,
+                    Age = model.Age,
+                    Email = model.Email,
+                };
+
+                IdentityResult result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                } else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Cinema/ViewModels/CreateModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.ViewModels
+{
+	public class CreateModel
+	{
+		[Required] public string Name { get; set; }
+		[Required] public string Email { get; set; }
+		[Required] public int Age { get; set; }
+		[Required] public string Password { get; set; }
+	}
+}
